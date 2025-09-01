17d9f040db37a209e0ccea6c079288659b93a831
Diana Webster <diana.webster380@gmail.com>
2025-09-01 17:12:29 +0100

    added a blazor webapp for the front-end logic and .net core api to replace the mvc application , added filtering action on the user list

---
+++ UserManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.API.Models.Users;
+using UserManagement.Services.Domain.Interfaces;
+
+
+namespace UserManagement.API.Controllers;
+
+
+[Route("users")]
+[ApiController]
+public class UsersController : Controller
+{
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService) => _userService = userService;
+
+    [HttpGet]
+    [Route("GetAll")]
+    public async Task<IActionResult> GetAll()
+    {
+        // Simulate async operation if _userService.GetAll() is synchronous
+        var items = await Task.Run(() =>
+            _userService.GetAll().Select(p => new UserListItemViewModel
+            {
+                Id = p.Id,
+                Forename = p.Forename,
+                Surname = p.Surname,
+                Email = p.Email,
+                IsActive = p.IsActive
+            }).ToList()
+        );
+
+        return Ok(items);
+    }
+}
